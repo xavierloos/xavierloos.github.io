5e03b8276820ac60be645c7124dd36f0e5650e1e
xavierloos <xavier.loos04@gmail.com>
2021-03-07 13:04:35 +0000

    Contact form

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -25,61 +25,16 @@
 <?php
 
 if (isset($_POST['submit'])) {
-  $name = $_POST['name']; // Get Name value from HTML Form
-  $email_id = $_POST['email']; // Get Email Value
-  $subject = $_POST['subject'];
-  $msg = $_POST['message']; // Get Message Value
+    $name = $_POST['name']; // Get Name value from HTML Form
+    $email = $_POST['email']; // Get Email Value
+    $subject = $_POST['subject'];
+    $message = $_POST['message']; // Get Message Value
 
-
-  $to = "xavier.loos04@gmail.com"; // You can change here your Email
-  $subject = "'$name' has been sent a mail"; // This is your subject
-
-  // HTML Message Starts here
-  $message = "
-        <html>
-            <body>
-                <table style='width:600px;'>
-                    <tbody>
-                        <tr>
-                            <td style='width:150px'><strong>Name: </strong></td>
-                            <td style='width:400px'>$name</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Email ID: </strong></td>
-                            <td style='width:400px'>$email_id</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Email ID: </strong></td>
-                            <td style='width:400px'>$subject</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Message: </strong></td>
-                            <td style='width:400px'>$msg</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </body>
-        </html>
-        ";
-  // HTML Message Ends here
-
-  // Always set content-type when sending HTML email
-  $headers = "MIME-Version: 1.0" . "\r\n";
-  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-  // More headers
-  $headers .= 'From: Javier Lopez Ramos <javierlr.dev@gmail.com>' . "\r\n"; // Give an email id on which you want get a reply. User will get a mail from this email id
-  if (mail($to, $subject, $msg, $headers)) {
-    // Message if mail has been sent
-    echo "<script>
-                alert('Mail has been sent Successfully.');
-            </script>";
-  } else {
-    // Message if mail has been not sent
-    echo "<script>
-                alert('EMAIL FAILED');
-            </script>";
-  }
+    $mailTo = "javierlr.dev4@gmail.com"; // You can change here your Email
+    $headers = 'From: '.$email; // Give an email id on which you want get a reply.
+    $txt = "You have received an email from " . $name . ".\n\n" . $message;
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
 }
-header("Location: ../index.js");
+
 ?>
