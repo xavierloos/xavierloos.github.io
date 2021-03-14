f4430f5e13c7974bc54466dbfe7b02cade2e2ea9
xavierloos <xavier.loos04@gmail.com>
2021-03-14 15:43:07 +0000

    responsive offset

--- js/smoth_scroll.js
+++ js/smoth_scroll.js
@@ -6,15 +6,17 @@ jQuery(document).ready(($) => {
     e.preventDefault();
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
+      var screenwidth
+      if ($(window).width() < 960) {
+        screenwidth = 0
+      } else {
+        screenwidth = 62
+      }
       if (target.length) {
-
         $('html, body').animate({
-          scrollTop: target.offset().top - 62
+          scrollTop: target.offset().top - screenwidth
         }, 1500, 'easeInOutExpo');
       }
     }
   });
-  // Select all links with hashes
-
-
 });
